a0118114804f638b962fe75503f57bde61cc7e69
毛心洁 <maoxinjie@maoxinjiedeMacBook-Pro.local>
2021-03-17 14:34:21 +0800

    manipulation.py

--- Practical6/manipulation.py
+++ Practical6/manipulation.py
@@ -1,2 +1,12 @@
-gene_lengths=[9410,394141,4442,105338,19149,76779,126550,36296,842,14981]
-exon_counts=[51,1142,42,316,25,650,32533,57,1,523]
+import numpy as np
+gene_lengths=np.array([9410,394141,4442,105338,19149,76779,126550,36296,842,14981])
+exon_counts=np.array([51,1142,42,316,25,650,32533,57,1,523])
+average_exon_length= gene_lengths / exon_counts
+#measure the average exon length across all 10 genes
+average_exon_length.sort()
+#sort the values for the average exon length across all 10 genes
+print(average_exon_length)
+import matplotlib.pyplot as plt
+plt.boxplot(average_exon_length)
+plt.show()
+
