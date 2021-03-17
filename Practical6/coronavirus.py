4a6b8611c19107afea151e5578b81130bf0516df
毛心洁 <maoxinjie@maoxinjiedeMacBook-Pro.local>
2021-03-17 13:44:09 +0800

    coronavirus.py

---
+++ Practical6/coronavirus.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+labels = 'USA', 'India', 'Brazil', 'Russia', 'UK'
+sizes = [29862124, 11285561, 11205972, 4360823, 4234924]
+#the countries are matched with their infection cases
+explode = (0, 0, 0, 0, 0)
+#there is no space in the whole pie
+plt.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', shadow=False, startangle=90)
+plt.axis('equal')
+plt.show()
