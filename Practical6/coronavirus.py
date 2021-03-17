2dbf3bfc7bf57ba51c4caa5d3d6b4b4672db2099
毛心洁 <maoxinjie@maoxinjiedeMacBook-Pro.local>
2021-03-17 15:41:45 +0800

    coronavirus.py

--- Practical6/coronavirus.py
+++ Practical6/coronavirus.py
@@ -2,10 +2,11 @@ import matplotlib.pyplot as plt
 labels = 'USA', 'India', 'Brazil', 'Russia', 'UK'
 sizes = [29862124, 11285561, 11205972, 4360823, 4234924]
 #the countries are matched with their infection cases
+a=sum(sizes)
 explode = (0, 0, 0, 0, 0)
 #there is no space in the whole pie
 plt.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', shadow=False, startangle=90)
 plt.axis('equal')
 plt.show()
-frequency_dictionary={'USA':49.0, 'India':18.5, 'Brazil':18.4, 'Russia':7.2, 'UK':6.9}
+frequency_dictionary={'USA':sizes[0]/a, 'India':sizes[1]/a, 'Brazil':sizes[2]/a, 'Russia':sizes[3]/a, 'UK':sizes[4]/a}
 print(frequency_dictionary)
