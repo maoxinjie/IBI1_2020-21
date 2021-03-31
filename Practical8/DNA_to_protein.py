0275a8383a19f8cf00ba280626619683e756c1ea
毛心洁 <maoxinjie@maoxinjiedeMacBook-Pro.local>
2021-04-01 00:50:40 +0800

    DNA_to_protein.py

---
+++ Practical8/DNA_to_protein.py
@@ -0,0 +1,21 @@
+seq = 'ATGCGACTACGATCGAGGGCC'
+genes = {'TTT':'F', 'TCT':'S', 'TAT':'Y', 'TGT':'C',
+'TTC':'F', 'TCC':'S', 'TAC':'Y', 'TGC':'C',
+'TTA':'L', 'TCA':'S', 'TAA':'O', 'TGA':'X',
+'TTG':'L', 'TCG':'S', 'TAG':'U', 'TGG':'W',
+'CTT':'L', 'CCT':'P', 'CAT':'H', 'CGT':'R',
+'CTC':'L', 'CCC':'P', 'CAC':'H', 'CGC':'R',
+'CTA':'L', 'CCA':'P', 'CAA':'Q', 'CGA':'R',
+'CTG':'L', 'CCG':'P', 'CAG':'Z', 'CGG':'R',
+'ATT':'I', 'ACT':'T', 'AAT':'N', 'AGT':'S',
+'ATC':'I', 'ACC':'T', 'AAC':'B', 'AGC':'S',
+'ATA':'J', 'ACA':'T', 'AAA':'K', 'AGA':'R',
+'ATG':'M', 'ACG':'T', 'AAG':'K', 'AGG':'R',
+'GTT':'V', 'GCT':'A', 'GAT':'D', 'GGT':'G',
+'GTC':'V', 'GCC':'A', 'GAC':'D', 'GGC':'G',
+'GTA':'V', 'GCA':'A', 'GAA':'E', 'GGA':'G',
+'GTG':'V', 'GCG':'A', 'GAG':'E', 'GGG':'G',}
+protein = ''
+for i in range(0, len(seq), 3):
+    protein = protein + genes[seq[i:(i+3)]]
+print(protein)
