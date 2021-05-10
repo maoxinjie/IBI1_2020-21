54f5794c5a9e9ad0ae71d697fa4dca6ed915c139
maoxinjie <79959138+maoxinjie@users.noreply.github.com>
2021-05-10 09:36:56 +0800

    Update coronavirus.py

--- Practical6/coronavirus.py
+++ Practical6/coronavirus.py
@@ -7,6 +7,7 @@ explode = (0, 0, 0, 0, 0)
 #there is no space in the whole pie
 plt.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', shadow=False, startangle=90)
 plt.axis('equal')
+plt.title('Comparing coronavirus infection rates across countries')
 plt.show()
 frequency_dictionary={'USA':sizes[0]/a, 'India':sizes[1]/a, 'Brazil':sizes[2]/a, 'Russia':sizes[3]/a, 'UK':sizes[4]/a}
 print(frequency_dictionary)
