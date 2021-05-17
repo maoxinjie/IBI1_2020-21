9b5e2412e73896177b49bcd5f1f1972320a9dbd2
maoxinjie <79959138+maoxinjie@users.noreply.github.com>
2021-05-17 12:43:37 +0800

    Update get_unknown_function.py

--- Practical8/get_unknown_function.py
+++ Practical8/get_unknown_function.py
@@ -1,42 +1,28 @@
 import re
 a = open('unknown_function.fa', 'w')
 Saccharomyces = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa','r')
-genes = {'TTT':'F', 'TCT':'S', 'TAT':'Y', 'TGT':'C',
-'TTC':'F', 'TCC':'S', 'TAC':'Y', 'TGC':'C',
-'TTA':'L', 'TCA':'S', 'TAA':'O', 'TGA':'X',
-'TTG':'L', 'TCG':'S', 'TAG':'U', 'TGG':'W',
-'CTT':'L', 'CCT':'P', 'CAT':'H', 'CGT':'R',
-'CTC':'L', 'CCC':'P', 'CAC':'H', 'CGC':'R',
-'CTA':'L', 'CCA':'P', 'CAA':'Q', 'CGA':'R',
-'CTG':'L', 'CCG':'P', 'CAG':'Z', 'CGG':'R',
-'ATT':'I', 'ACT':'T', 'AAT':'N', 'AGT':'S',
-'ATC':'I', 'ACC':'T', 'AAC':'B', 'AGC':'S',
-'ATA':'J', 'ACA':'T', 'AAA':'K', 'AGA':'R',
-'ATG':'M', 'ACG':'T', 'AAG':'K', 'AGG':'R',
-'GTT':'V', 'GCT':'A', 'GAT':'D', 'GGT':'G',
-'GTC':'V', 'GCC':'A', 'GAC':'D', 'GGC':'G',
-'GTA':'V', 'GCA':'A', 'GAA':'E', 'GGA':'G',
-'GTG':'V', 'GCG':'A', 'GAG':'E', 'GGG':'G',}
-# input the dictionary
-protein = ''
 recording = False
-protein_map = {}
-cur_seq = ""
+gene_map = {}
 gene_name = ""
+gene_sequence = ""
+# set the initial dictionary and list
 for line in Saccharomyces:
     if  line.startswith('>'):
-        # select the lines that start with the '>'
+        # select each gene for it begins with a ">"
         if recording:
-            protein_map[gene_name] = cur_seq
+            gene_map[gene_name] = gene_sequence
+            # in the dictionary 'gene_map', gene_name is related to the gene_sequence 
         recording = 'unknown function' in line
+        # select the gene with unknown function
         gene_name = re.findall(r'gene:(\S+)', line)[0]
-        # find the name of the gene in the line
-        cur_seq = ""
+        # find all the gene_name
+        gene_sequence = ""
     elif recording:
-        for i in range(0, len(line)-1, 3):
-            # the \n is one character, so it is len(line)-1
-            cur_seq = cur_seq + genes[line[i:(i+3)]]
-for gene_name, cur_seq in protein_map.items():
-    print(f'{gene_name:15}  {len(cur_seq)}\n{cur_seq}')
-    a.write(f'{gene_name:15}  {len(cur_seq)}\n{cur_seq}\n')
+        for i in range(0, len(line)-1):
+            # the '\n' is one character, so I set the range between 0 to len(line)-1
+            gene_sequence = gene_sequence + line[i:i+1]
+for gene_name, gene_sequence in gene_map.items():
+    print(f'{gene_name:15}  {gene_sequence}')
+    # set the space of 15 character.
+    a.write(f'{gene_name:15}  {gene_sequence}\n')
 a.close()
